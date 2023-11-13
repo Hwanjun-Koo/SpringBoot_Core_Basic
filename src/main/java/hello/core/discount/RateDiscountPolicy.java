ecc51fe3e1b54e59f1d7f17f12eb5260406ac2c6
구환준/모건 <koojun99@naver.com>
2023-11-13 23:43:07 +0900

    Ch 3 - 새로운 할인 정책 도입

---
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -0,0 +1,17 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy{
+
+    private int discountPercent = 10; // 10% 할인
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return price * discountPercent / 100;
+        } else {
+            return 0;
+        }
+    } //영한좌의 꿀팁: Ctrl + Shift + T로 테스트 클래스를 빠르게 생성할 수 있다.
+}
