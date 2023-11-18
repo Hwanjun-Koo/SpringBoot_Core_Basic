7a6fdddee06a9fcb351e7b89fc3d6810dc8ec038
구환준/모건 <koojun99@naver.com>
2023-11-18 22:43:06 +0900

    Ch 3 - AppConfig Refactor

--- src/main/java/hello/core/Appconfig.java
+++ src/main/java/hello/core/Appconfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -10,10 +12,18 @@ import hello.core.order.OrderServiceImpl;
 public class Appconfig { //전체 애플리케이션의 구성
 
     public MemberService memberService() { //회원 서비스 객체 생성
-        return new MemberServiceImpl(new MemoryMemberRepository()); //MemoryMemberRepository를 생성자를 통해 넣어줌(생성자 주입)
+        return new MemberServiceImpl(memberRepository()); //MemoryMemberRepository를 생성자를 통해 넣어줌(생성자 주입)
+    }
+
+    private static MemberRepository memberRepository() {//회원 저장소 객체 생성
+        return new MemoryMemberRepository(); //나중에 DB로 바꾸고 싶다면 여기만 바꾸면 됨
     }
 
     public OrderService orderService() { //주문 서비스 객체 생성
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy()); //생성자 주입
+        return new OrderServiceImpl(memberRepository(), discountPolicy()); //생성자 주입
+    }
+
+    public DiscountPolicy discountPolicy() { //할인 정책 객체 생성
+        return new FixDiscountPolicy(); //나중에 할인 정책을 변경하고 싶다면 여기만 바꾸면 됨
     }
 }
