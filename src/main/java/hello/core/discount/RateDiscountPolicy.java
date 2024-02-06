da7574b894e5f2fb0c9d71d22580884721cb1378
구환준/모건 <koojun99@naver.com>
2024-02-06 16:50:19 +0900

    Ch 7 - 어노테이션 직접 만들기

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -1,5 +1,6 @@
 package hello.core.discount;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -8,10 +9,12 @@ import org.springframework.stereotype.Component;
 
 @Component
 //@Qualifier("mainDiscountPolicy")
-@Primary
-public class RateDiscountPolicy implements DiscountPolicy{
+//@Primary
+@MainDiscountPolicy
+public class RateDiscountPolicy implements DiscountPolicy {
 
     private int discountPercent = 10; // 10% 할인
+
     @Override
     public int discount(Member member, int price) {
         if (member.getGrade() == Grade.VIP) {
