8307881aed1925ceec65aa4a9b00f296c733b8e9
구환준/모건 <koojun99@naver.com>
2024-04-01 22:18:33 +0900

    Ch 9 - 웹 스코프와 프록시

--- src/main/java/hello/core/common/MyLogger.java
+++ src/main/java/hello/core/common/MyLogger.java
@@ -3,12 +3,13 @@ package hello.core.common;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
 
 @Component
-@Scope(value ="request")
+@Scope(value ="request", proxyMode = ScopedProxyMode.TARGET_CLASS) // MyLogger를 사용하는 클라이언트가 MyLogger의 가짜 프록시 클래스를 주입받게 된다.
 public class MyLogger {
 
     private String uuid;
--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -13,15 +13,16 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
     @ResponseBody //http 응답 바디에 데이터를 직접 넣어주겠다.
     public String logDemo(HttpServletRequest request) throws InterruptedException {
         String requestURL = request.getRequestURL().toString(); //요청 URL 가져오기
-        MyLogger myLogger = myLoggerProvider.getObject();
 
+        System.out.println("myLogger = " + myLogger.getClass());
         myLogger.setRequestURL(requestURL);
+
         myLogger.log("controller test");
         Thread.sleep(1000);
         logDemoService.logic("testId");
